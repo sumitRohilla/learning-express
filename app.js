64114cab4e4b0ebb98542f6d5150139f089a44c9
unknown <sumitrohilla171996@gmail.com>
2024-06-13 11:33:45 +0530

    updated

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import logger from "./src/middlewares/logger.js";
 import errorHandler from "./src/middlewares/error.js";
 import pageNotFound from "./src/middlewares/notFound.js";
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || "";
 
 const app = express();
 
